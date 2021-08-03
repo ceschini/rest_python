d0158aa46bc5e584e367d710d7cd234b96a3bc12
ceschini <lucas.ceschini@pixforce.ai>
2021-08-03 15:03:06 -0300

    hotel model criado

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
     {
@@ -53,7 +54,7 @@ class Hotel(Resource):
     def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
 
-        novo_hotel = {'hotel_id': hotel_id, **dados}
+        novo_hotel = HotelModel(hotel_id, **dados).json()
 
         hoteis.append(novo_hotel)
         return novo_hotel, 200  # OK
@@ -61,7 +62,7 @@ class Hotel(Resource):
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
 
-        novo_hotel = {'hotel_id': hotel_id, **dados}
+        novo_hotel = HotelModel(hotel_id, **dados).json()
 
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
