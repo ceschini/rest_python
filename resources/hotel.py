e3c73f9e5fff7363ea2c12e0ac9c0158bb4c295a
ceschini <lucas.ceschini@pixforce.ai>
2021-08-04 11:17:23 -0300

    banco de dados e tratamento de erros

--- resources/hotel.py
+++ resources/hotel.py
@@ -28,52 +28,60 @@ hoteis = [
 
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': hoteis}
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
 
 
 class Hotel(Resource):
     # definindo construtor de argumentos
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
+    argumentos.add_argument('nome', type=str, required=True,
+                            help="The field 'nome' cannot be left blank")
+    argumentos.add_argument('estrelas', type=float, required=True,
+                            help="The field 'estrelas' cannot be left blank")
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
-
     def get(self, hotel_id):
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return {'message': 'Hotel not found.'}, 404  # not found
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            # 400 = bad request
+            return {'message': "Hotel id '{}' already exists.".format(hotel_id)}, 400
+
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = HotelModel(hotel_id, **dados).json()
-
-        hoteis.append(novo_hotel)
-        return novo_hotel, 200  # OK
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+            return hotel.json()
+        except:
+            # 500 = internal server error
+            return {'message': 'An error occurred while trying to save data.'}, 500
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = HotelModel(hotel_id, **dados).json()
-
-        hotel = Hotel.find_hotel(hotel_id)
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200  # OK
-
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201  # created
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200  # OK
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+            return hotel.json(), 201  # created
+        except:
+            # 500 = internal server error
+            return {'message': 'An error occurred while trying to save data.'}, 500
 
     def delete(self, hotel_id):
-        # list comprehension
-        global hoteis  # referenciando a lista de hoteis
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-        return {'message': 'Hotel deleted.'}, 200
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+                return {'message': 'Hotel deleted.'}, 200
+            except:
+                return {'message': 'An error occurred while trying to delete data.'}, 500
+        return {'message': 'Hotel not found.'}, 404
